459d5e487c294a9b4471d460c47d916a92fc3307
caoyangjiang <caoyang@hypevr.com>
2017-01-17 11:56:28 -0800

    - alembic reader in progress

--- apps/AlembicReader/src/AlembicReader.cpp
+++ apps/AlembicReader/src/AlembicReader.cpp
@@ -8,8 +8,45 @@
 #include <Alembic/Util/All.h>
 
 #include <array>
+#include <fstream>
 #include <string>
 #include <vector>
+
+void SaveObjFile(const std::string& filename,
+                 const std::string& mtlname,
+                 const std::vector<float>& vb,
+                 const std::vector<float>& uvb,
+                 const std::vector<int>& idxb)
+{
+  std::ofstream vstream(
+      filename,
+      std::ofstream::out | std::ofstream::binary | std::ofstream::trunc);
+  vstream << "mtllib " << mtlname << std::endl;
+
+  for (size_t i = 0; i < vb.size(); i += 3)
+  {
+    vstream << "v"
+            << " " << vb[i] << " " << vb[i + 1] << " " << vb[i + 2]
+            << std::endl;
+  }
+
+  for (size_t i = 0; i < uvb.size(); i += 2)
+  {
+    vstream << "vt"
+            << " " << uvb[i] << " " << uvb[i + 1] << " " << std::endl;
+  }
+
+  for (size_t i = 0; i < idxb.size(); i += 3)
+  {
+    vstream << "f"
+            << " " << idxb[i] + 1 << "/" << idxb[i] + 1 << " "
+            << idxb[i + 1] + 1 << "/" << idxb[i + 1] + 1 << " "
+            << idxb[i + 2] + 1 << "/" << idxb[i + 2] + 1 << std::endl;
+  }
+
+  vstream.close();
+}
+
 class AlembicReader
 {
  public:
@@ -17,15 +54,55 @@ class AlembicReader
   {
   }
 
+  /**
+   * @brief      Open alembic file
+   *
+   * @param[in]  filename  The filename
+   *
+   * @return     return true if file is valid
+   */
   bool Open(const std::string& filename)
   {
     Alembic::AbcCoreOgawa::ALEMBIC_VERSION_NS::ReadArchive reader;
-    readerptr_    = reader(filename);
+    readerptr_ = reader(filename);
+
+    if (readerptr_ == nullptr)
+    {
+      std::cout << "[ERROR]: Reading alembic file failed." << std::endl;
+      return false;
+    }
+
     objreaderptr_ = readerptr_->getTop();
+
+    if (objreaderptr_ == nullptr)
+    {
+      std::cout << "[ERROR]: Getting object reader failed." << std::endl;
+      return false;
+    }
 
     return true;
   }
 
+  /**
+   * @brief      Gets the geometry by geometry ID and frame ID. To avoid extra
+   *             copying, the vertex sample and uv sample are temporarily saved.
+   *             However, calling GetGeoemetry() again will destroy the previous
+   *             samples. Upon return, user needs to test if uv buffer pointer
+   *             is nullptr to determine validity.
+   *
+   * @param[in]  frameid        The frame ID
+   * @param[in]  geometryid     The geometry ID
+   * @param      idxbuffer      The index buffer
+   * @param      verbuffer      The vertex buffer
+   * @param      uvbuffer       The uv buffer
+   * @param      verbuffersize  The vertex buffer size
+   * @param      idxbuffersize  The index buffer size
+   * @param      facecount      The face count
+   * @param      dim            The dimension of the bounding box
+   * @param      center         The center of the bounding box
+   *
+   * @return     The geometry.
+   */
   bool GetGeometry(int frameid,
                    int geometryid,
                    const int*& idxbuffer,
@@ -53,16 +130,28 @@ class AlembicReader
       return false;
     }
 
+    if (vsample_.getFaceCounts()->get()[0] != 3)
+    {
+      std::cout << "[ERROR]: mesh face has "
+                << vsample_.getFaceCounts()->get()[0]
+                << " vertices. Not triangle mesh." << std::endl;
+      return false;
+    }
+
     facecount = static_cast<int>(vsample_.getFaceCounts()->size());
     verbuffersize =
         static_cast<int>(vsample_.getPositions()->size()) * sizeof(float) * 3;
     idxbuffersize =
-        static_cast<int>(vsample_.getFaceIndices()->size()) * sizeof(float) * 3;
+        static_cast<int>(vsample_.getFaceIndices()->size()) * sizeof(int);
     idxbuffer = vsample_.getFaceIndices()->get();
     verbuffer = vsample_.getPositions()->get()->getValue();
 
     // Get bounding box
     Alembic::Abc::ALEMBIC_VERSION_NS::Box3d box = vsample_.getSelfBounds();
+
+    std::cout << meshobject.getTopologyVariance() << std::endl;
+    std::cout << box.center()[0] << " " << box.center()[1] << " "
+              << box.center()[2] << std::endl;
 
     if (dim != nullptr)
     {
@@ -74,8 +163,8 @@ class AlembicReader
     if (center != nullptr)
     {
       (*center)[0] = box.size()[0];
-      (*center)[0] = box.size()[1];
-      (*center)[0] = box.size()[2];
+      (*center)[1] = box.size()[1];
+      (*center)[2] = box.size()[2];
     }
 
     // Get UVs
@@ -107,12 +196,18 @@ class AlembicReader
     }
     else
     {
+      std::cout << "[WARNING]: Mesh has no UV." << std::endl;
       uvbuffer = nullptr;
     }
 
     return true;
   }
 
+  /**
+   * @brief      Gets the readable geometry ID
+   *
+   * @return     The geometry IDs.
+   */
   std::vector<std::string> GetGeometryIDs() const
   {
     std::vector<std::string> geometryids;
@@ -126,6 +221,14 @@ class AlembicReader
     return geometryids;
   }
 
+  /**
+   * @brief      Query the number of frames for the geometry. User must use
+   *             GetNumOfGeometries() to get the range of valid geometry IDs.
+   *
+   * @param[in]  geometryid  id  The geometry id
+   *
+   * @return     The number of frames.
+   */
   int GetNumOfFrames(int geometryid) const
   {
     Alembic::Abc::ALEMBIC_VERSION_NS::IObject topobject(readerptr_->getTop());
@@ -134,6 +237,11 @@ class AlembicReader
     return static_cast<int>(meshobject.getNumSamples());
   }
 
+  /**
+   * @brief      Gets the number of geometries.
+   *
+   * @return     The number of geometries.
+   */
   int GetNumOfGeometries() const
   {
     Alembic::Abc::ALEMBIC_VERSION_NS::IObject topobject(readerptr_->getTop());
@@ -179,79 +287,46 @@ int main(int argc, char** argv)
   const int* idxbuffer;
   const float* vbuffer;
   const float* uvbuffer;
-  int vercount;
-  int idxcount;
+
+  int vsize;
+  int idxsize;
   int facecount;
 
   if (!reader.GetGeometry(
-          0, 0, idxbuffer, vbuffer, uvbuffer, vercount, idxcount, facecount))
+          0, 0, idxbuffer, vbuffer, uvbuffer, vsize, idxsize, facecount))
   {
     std::cout << "GetGeometry failed" << std::endl;
     return false;
   }
-
-  std::cout << vbuffer[0] << std::endl;
-  std::cout << vbuffer[1] << std::endl;
-  std::cout << vbuffer[2] << std::endl;
-  std::cout << "Vercount: " << vercount << std::endl;
-  std::cout << "Idxcount: " << idxcount << std::endl;
+  std::cout << "Vercount: " << vsize << std::endl;
+  std::cout << "Idxcount: " << idxsize << std::endl;
   std::cout << "facecount: " << facecount << std::endl;
+
+  std::vector<float> uvs;
+  std::vector<float> vertices;
+  std::vector<int> indices;
+
+  uvs.resize(vsize / 4);
+  vertices.resize(vsize / 4);
+  indices.resize(idxsize / 4);
+
+  for (int i = 0; i < vsize / 4; i++)
+  {
+    vertices[i] = vbuffer[i];
+  }
+
+  for (int i = 0; i < vsize / 4; i++)
+  {
+    uvs[i] = uvbuffer[i];
+  }
+
+  for (int i = 0; i < idxsize / 4; i++)
+  {
+    indices[i] = idxbuffer[i];
+  }
+
+  SaveObjFile(
+      std::string("test.obj"), std::string("test.mtl"), vertices, uvs, indices);
 
   return 0;
 }
-
-// int main(int argc, char** argv)
-// {
-//   if (argc < 2) return -1;
-
-//   Alembic::AbcCoreOgawa::ALEMBIC_VERSION_NS::ReadArchive reader;
-//   Alembic::AbcCoreAbstract::ArchiveReaderPtr readerptr =
-//       reader(std::string(argv[1]));
-//   Alembic::AbcCoreAbstract::ObjectReaderPtr objreaderptr =
-//   readerptr->getTop();
-
-//   Alembic::Abc::ALEMBIC_VERSION_NS::IObject topobject(readerptr->getTop());
-//   std::cout << readerptr->getNumTimeSamplings() << std::endl;
-//   std::cout << readerptr->getMaxNumSamplesForTimeSamplingIndex(0) <<
-//   std::endl;
-//   std::cout << readerptr->getMaxNumSamplesForTimeSamplingIndex(1) <<
-//   std::endl;
-//   std::cout << topobject.getName() << std::endl;
-//   std::cout << topobject.getNumChildren() << std::endl;
-//   std::cout << topobject.getChild(0).getName() << std::endl;
-//   std::cout << topobject.getChild(0).getNumChildren() << std::endl;
-
-//   Alembic::AbcGeom::IPolyMeshSchema meshobject(
-//       topobject.getChild(0).getChild(0).getProperties());
-//   std::cout << meshobject.getNumSamples() << std::endl;
-//   int64_t idx = 119;
-//   Alembic::Abc::ALEMBIC_VERSION_NS::ISampleSelector selector(idx);
-//   Alembic::AbcGeom::IPolyMeshSchema::Sample sample;
-
-//   meshobject.get(sample, selector);
-//   Alembic::Abc::ALEMBIC_VERSION_NS::Box3d box = sample.getSelfBounds();
-//   std::cout << sample.getFaceCounts()->size() << std::endl;
-//   std::cout << sample.getFaceIndices()->size() << std::endl;
-//   std::cout << box.size()[0] << std::endl;
-//   std::cout << box.size()[1] << std::endl;
-//   std::cout << box.size()[2] << std::endl;
-
-//   std::cout << box.center()[0] << std::endl;
-//   std::cout << box.center()[1] << std::endl;
-//   std::cout << box.center()[2] << std::endl;
-//   std::cout << sample.getPositions()->size() << std::endl;
-//   // std::cout << sample.getVelocities()->size() << std::endl;
-//   std::cout << sample.getPositions()->get()[0] << std::endl;
-//   std::cout << sample.getPositions()->get()->getValue()[0] << std::endl;
-//   std::cout << sample.getFaceCounts()->get()[0] << std::endl;
-//   std::cout << sample.getFaceCounts()->get()[1] << std::endl;
-//   std::cout << sample.getFaceCounts()->get()[2] << std::endl;
-//   std::cout << sample.getFaceCounts()->get()[50] << std::endl;
-//   std::cout << sample.getFaceIndices()->get()[0] << std::endl;
-//   std::cout << sample.getFaceIndices()->get()[1] << std::endl;
-//   std::cout << sample.getFaceIndices()->get()[2] << std::endl;
-//   std::cout << sample.getFaceIndices()->get()[3] << std::endl;
-//   std::cout << sample.getFaceIndices()->get()[4] << std::endl;
-//   std::cout << sample.getFaceIndices()->get()[5] << std::endl;
-//   return 0;
-// }
